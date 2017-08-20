1396ec696ae58cbf30611ec328aa2065aff3307c
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2017-08-20 15:33:59 +0000

    add acts_as_votable gem and setup

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'simple_form', '~> 3.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'devise', '~> 4.3'
 gem 'paperclip', '~> 5.1'
+gem 'masonry-rails', '~> 0.2.4'
+gem 'acts_as_votable', '~> 0.10.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,8 +1,8 @@
 class PicsController < ApplicationController
 
 
-before_action :find_pic, only: [:show, :edit, :update, :destroy]
-
+before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+before_action :authenticate_user!, except: [:index, :show]
 	def index
 		@pics = Pic.all.order("created_at DESC")
 	end
@@ -39,6 +39,11 @@ before_action :find_pic, only: [:show, :edit, :update, :destroy]
 		@pic.destroy
 		redirect_to root_path, notice: "Deleted Successfully!"
 	end
+
+	def upvote
+		@pic.upvote_by current_user
+		redirect_to :back
+	end 
 
 
 	private
